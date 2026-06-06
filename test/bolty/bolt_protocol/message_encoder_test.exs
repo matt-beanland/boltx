7afc3e836d1d66137251e115b3f0a3fdf78d79bc
Matt Beanland <beanland@live.com.au>
2026-06-06 15:55:51 +0930

    test: update message encoder tests from Bolt 3.x/2.x to 5.x
    DISCARD and PULL used to test the now-removed <= 3.0 fallback clauses.
    Updated all version references to 5.0 (lowest supported); DISCARD and
    PULL now assert on the signature byte only since the 4.0+ format
    includes n/qid parameters.

--- test/bolty/bolt_protocol/message_encoder_test.exs
+++ test/bolty/bolt_protocol/message_encoder_test.exs
@@ -24,63 +24,63 @@ defmodule Bolty.BoltProtocol.Message.MessageEncoderTest do
 
   describe "Encode HELLO" do
     test "without params" do
-      assert <<0x0, _, 0xB1, 0x1, _::binary>> = HelloMessage.encode(3.0, [])
+      assert <<0x0, _, 0xB1, 0x1, _::binary>> = HelloMessage.encode(5.0, [])
     end
 
     test "with params" do
       assert <<0x0, _, 0xB1, 0x1, _::binary>> =
-               HelloMessage.encode(3.0, user_agent: "user_agent_test")
+               HelloMessage.encode(5.0, user_agent: "user_agent_test")
     end
 
     test "with auth" do
       assert <<_, _, _, 0x01, _::binary>> =
-               HelloMessage.encode(3.0, auth: [username: "hello", password: "hellopw"])
+               HelloMessage.encode(5.0, auth: [username: "hello", password: "hellopw"])
     end
   end
 
   describe "Encode BEGIN" do
     test "without params" do
-      assert <<_, _, _, 0x11, _::binary>> = BeginMessage.encode(3.0, %{})
+      assert <<_, _, _, 0x11, _::binary>> = BeginMessage.encode(5.0, %{})
     end
 
     test "with params" do
-      assert <<_, _, _, 0x11, _::binary>> = BeginMessage.encode(3.0, %{mode: "w"})
+      assert <<_, _, _, 0x11, _::binary>> = BeginMessage.encode(5.0, %{mode: "w"})
     end
   end
 
   describe "Encode COMMIT" do
     test "without params" do
-      assert <<0x0, 0x2, 0xB0, 0x12, 0x0, 0x0>> == CommitMessage.encode(3.0)
+      assert <<0x0, 0x2, 0xB0, 0x12, 0x0, 0x0>> == CommitMessage.encode(5.0)
     end
   end
 
   describe "Encode DISCARD" do
     test "without params" do
-      assert <<0x0, 0x2, 0xB0, 0x2F, 0x0, 0x0>> = DiscardMessage.encode(2.0, %{})
+      assert <<_, _, _, 0x2F, _::binary>> = DiscardMessage.encode(5.0, %{})
     end
   end
 
   describe "Encode GOODBYE" do
     test "without params" do
-      assert assert <<_, _, _, 0x02, _::binary>> = GoodbyeMessage.encode(3.0)
+      assert <<_, _, _, 0x02, _::binary>> = GoodbyeMessage.encode(5.0)
     end
   end
 
   describe "Encode ROLLBACK" do
     test "without params" do
-      assert <<0x0, 0x2, 0xB0, 0x13, 0x0, 0x0>> = RollbackMessage.encode(3.0)
+      assert <<0x0, 0x2, 0xB0, 0x13, 0x0, 0x0>> = RollbackMessage.encode(5.0)
     end
   end
 
   describe "Encode PULL" do
     test "without params" do
-      assert <<0x0, 0x2, 0xB0, 0x3F, 0x0, 0x0>> = PullMessage.encode(3.0, %{})
+      assert <<_, _, _, 0x3F, _::binary>> = PullMessage.encode(5.0, %{})
     end
   end
 
   describe "Encode RESET" do
     test "without params" do
-      assert <<0x0, 0x2, 0xB0, 0xF, 0x0, 0x0>> == ResetMessage.encode(3.0)
+      assert <<0x0, 0x2, 0xB0, 0xF, 0x0, 0x0>> == ResetMessage.encode(5.0)
     end
   end
 
@@ -89,7 +89,7 @@ defmodule Bolty.BoltProtocol.Message.MessageEncoderTest do
       assert <<0, 55, 179, 16, 143, 82, 69, 84, 85, 82, 78, 32, 49, 32, 65, 83, 32, 110, 117, 109,
                160, 164, 132, 109, 111, 100, 101, 129, 119, 137, 98, 111, 111, 107, 109, 97, 114,
                107, 115, 144, 130, 100, 98, 192, 139, 116, 120, 95, 109, 101, 116, 97, 100, 97,
-               116, 97, 192, 0, 0>> == RunMessage.encode(3.0, "RETURN 1 AS num", %{}, %{})
+               116, 97, 192, 0, 0>> == RunMessage.encode(5.0, "RETURN 1 AS num", %{}, %{})
     end
 
     test "with params" do
@@ -97,7 +97,7 @@ defmodule Bolty.BoltProtocol.Message.MessageEncoderTest do
                32, 110, 117, 109, 161, 131, 110, 117, 109, 5, 164, 132, 109, 111, 100, 101, 129,
                119, 137, 98, 111, 111, 107, 109, 97, 114, 107, 115, 144, 130, 100, 98, 192, 139,
                116, 120, 95, 109, 101, 116, 97, 100, 97, 116, 97, 192, 0, 0>> ==
-               RunMessage.encode(3.0, "RETURN $num AS num", %{num: 5}, %{})
+               RunMessage.encode(5.0, "RETURN $num AS num", %{num: 5}, %{})
     end
 
     test "with parameters encoding a structure" do
@@ -110,7 +110,7 @@ defmodule Bolty.BoltProtocol.Message.MessageEncoderTest do
                195, 164, 132, 109, 111, 100, 101, 129, 119, 137, 98, 111, 111, 107, 109, 97, 114,
                107, 115, 144, 130, 100, 98, 192, 139, 116, 120, 95, 109, 101, 116, 97, 100, 97,
                116, 97, 192, 0, 0>> ==
-               RunMessage.encode(3.0, query, params, %{})
+               RunMessage.encode(5.0, query, params, %{})
     end
   end
 end
